a1fb9e86b552953d3e0096c9bcdf349823bd8112
Michal Krkoska <m.krkoska@student.tudelft.nl>
2022-12-07 21:37:45 +0100

    add token functionality

---
+++ authentication-and-member-microservice/src/main/java/nl/tudelft/sem/template/authmember/authentication/JwtUserDetailsService.java
@@ -0,0 +1,46 @@
+package nl.tudelft.sem.template.authmember.authentication;
+
+import nl.tudelft.sem.template.authmember.domain.db.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+/**
+ * User details service responsible for retrieving the user from the DB.
+ */
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final transient MemberRepository memberRepository;
+
+    @Autowired
+    public JwtUserDetailsService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * Loads user information required for authentication from the DB.
+     *
+     * @param memberId The username of the user we want to authenticate
+     * @return The authentication user information of that user
+     * @throws UsernameNotFoundException Username was not found
+     */
+    @Override
+    public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
+        var optionalUser = memberRepository.findByMemberId(memberId);
+
+        if (optionalUser.isEmpty()) {
+            throw new UsernameNotFoundException("User does not exist");
+        }
+
+        var user = optionalUser.get();
+
+        return new User(user.getMemberId(), user.getPassword().toString(),
+                new ArrayList<>()); // no authorities/roles
+    }
+}
