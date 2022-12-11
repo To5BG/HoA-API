e85f4e54ac25c5da457e4a2e3b282722b0a73c7d
Alperen Guncan <A.Guncan@student.tudelft.nl>
2022-12-11 01:33:23 +0100

    Update subclasses and add javadocs

--- voting-microservice/src/main/java/voting/domain/BoardElection.java
+++ voting-microservice/src/main/java/voting/domain/BoardElection.java
@@ -1,45 +1,43 @@
 package voting.domain;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import voting.domain.converter.CandidatesConverter;
 import voting.domain.converter.VotesConverter;
 
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
 import javax.persistence.Convert;
-import javax.persistence.ElementCollection;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapKeyColumn;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Entity
-@Data
-@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
+@DiscriminatorValue("0")
 public class BoardElection extends Election {
 
     private int amountOfWinners;
     private String status;
 
     @Convert(converter = CandidatesConverter.class)
-    private ArrayList<Integer> candidates;
+    private List<Integer> candidates;
 
-    @ElementCollection
-    @MapKeyColumn(name="name")
-    @Column(name="value")
-    @CollectionTable(name="example_attributes", joinColumns=@JoinColumn(name="example_id"))
-    //@Convert(converter = VotesConverter.class)
-    private Map<Integer, Integer> votes;
+    @Convert(converter = VotesConverter.class)
+    private HashMap<Integer, Integer> votes;
 
+    /**
+     * Create a board election
+     *
+     * @param name            Name of board election
+     * @param description     Description of board election
+     * @param hoaId           hoaID the board election refers to
+     * @param scheduledFor    Time object, when the election will start
+     * @param amountOfWinners Amount of winners for this board election
+     * @param candidates      List of member ids of board candidates
+     */
     public BoardElection(String name, String description, int hoaId, Time scheduledFor, int amountOfWinners,
                          ArrayList<Integer> candidates) {
         super(name, description, hoaId, scheduledFor);
@@ -48,17 +46,30 @@ public class BoardElection extends Election {
         status = "scheduled";
         votes = new HashMap<>();
     }
-    private boolean canParticipate(String memberId) {
+
+    /**
+     * {@inheritDoc}
+     */
+    private boolean canParticipate(Integer memberId) {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void vote(int membershipId, int voteChoice) {
-        if (this.status.equals("ongoing")) {
+        if (this.status.equals("ongoing") && canParticipate(membershipId)) {
             votes.put(membershipId, voteChoice);
             this.incrementVoteCount();
         }
     }
+
+    /**
+     * Calculates the outcome of a board election
+     *
+     * @return List of winners, capped if less than amountOfWinners
+     */
     public List<Integer> findOutcome() {
         return votes.values()
                 .stream()
@@ -66,6 +77,10 @@ public class BoardElection extends Election {
                 .limit(Math.min(amountOfWinners, candidates.size()))
                 .collect(Collectors.toList());
     }
+
+    /**
+     * {@inheritDoc}
+     */
     public List<Integer> conclude() {
         List<Integer> currentWinners = findOutcome();
         status = "finished";
--- voting-microservice/src/main/java/voting/domain/Proposal.java
+++ voting-microservice/src/main/java/voting/domain/Proposal.java
@@ -1,31 +1,32 @@
 package voting.domain;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import voting.domain.converter.VotesConverter;
 
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
+import javax.persistence.Convert;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapKeyColumn;
 import java.sql.Time;
 import java.util.HashMap;
 
 @Entity
-@Data
-@EqualsAndHashCode(callSuper = true)
+@DiscriminatorValue("1")
 @NoArgsConstructor
 public class Proposal extends Election {
     private boolean winningChoice;
     private String status;
-    @ElementCollection
-    @MapKeyColumn(name="name")
-    @Column(name="value")
-    @CollectionTable(name="example_attributes", joinColumns=@JoinColumn(name="example_id"))
+
+    @Convert(converter = VotesConverter.class)
     private HashMap<Integer, Boolean> votes;
 
+    /**
+     * Creates a proposal
+     *
+     * @param name         Name of proposal
+     * @param description  Description of proposal
+     * @param hoaId        hoaID the proposal refers to
+     * @param scheduledFor Time object, when the election will start
+     */
     public Proposal(String name, String description, int hoaId, Time scheduledFor) {
         super(name, description, hoaId, scheduledFor);
         winningChoice = false;
@@ -33,19 +34,45 @@ public class Proposal extends Election {
         votes = new HashMap<>();
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    private boolean canParticipate(Integer memberId) {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public void vote(int membershipId, boolean vote) {
-        if (status.equals("ongoing")) {
+        if (status.equals("ongoing") && canParticipate(membershipId)) {
             votes.put(membershipId, vote);
             this.incrementVoteCount();
         }
     }
+
+    /**
+     * Calculates the outcome of a proposal
+     *
+     * @return Binary decision, based on majority voting
+     */
     public boolean findOutcome() {
         Integer[] counts = votes.values().stream()
                 .collect(() -> new Integer[2],
-                        (acc, b) -> {if (b) acc[1]++; else acc[0]++;},
-                        (acc, acc2) -> {acc[0] += acc2[0]; acc[1] += acc2[1];});
+                        (acc, b) -> {
+                            if (b) acc[1]++;
+                            else acc[0]++;
+                        },
+                        (acc, acc2) -> {
+                            acc[0] += acc2[0];
+                            acc[1] += acc2[1];
+                        });
         return counts[0] <= counts[1];
     }
+
+    /**
+     * {@inheritDoc}
+     */
     public Boolean conclude() {
         winningChoice = findOutcome();
         status = "finished";
