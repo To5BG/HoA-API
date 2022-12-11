c7fbbce780be5895737caf37aedfd3a8bfb8b9a0
Alperen Guncan <A.Guncan@student.tudelft.nl>
2022-12-11 01:35:06 +0100

    Refactor candidate converter

--- voting-microservice/src/main/java/voting/domain/converter/CandidatesConverter.java
+++ voting-microservice/src/main/java/voting/domain/converter/CandidatesConverter.java
@@ -2,31 +2,23 @@ package voting.domain.converter;
 
 import javax.persistence.AttributeConverter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class CandidatesConverter implements AttributeConverter<ArrayList<Integer>, String> {
+public class CandidatesConverter implements AttributeConverter<List<Integer>, String> {
 
 	@Override
-	public String convertToDatabaseColumn(ArrayList<Integer> candidates) {
-		String result = "";
-		for (int l : candidates) {
-			result += l + ",";
-		}
-		if (result.length() != 0) {
-			result = result.substring(0, result.length() - 1);
-		}
-		return result;
+	public String convertToDatabaseColumn(List<Integer> candidates) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(candidates.stream().map(i -> i + ",").collect(Collectors.joining()));
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
 	}
 
 	@Override
-	public ArrayList<Integer> convertToEntityAttribute(String dbData) {
-		ArrayList<Integer> list = new ArrayList<>();
-		if (dbData.isEmpty()) {
-			return list;
-		}
-		String[] data = dbData.split(",");
-		for (String s : data) {
-			list.add(Integer.parseInt(s));
-		}
-		return list;
+	public List<Integer> convertToEntityAttribute(String dbData) {
+		if (dbData.isEmpty()) return new ArrayList<>();
+		return Arrays.stream(dbData.split(",")).map(Integer::parseInt).collect(Collectors.toList());
 	}
 }
