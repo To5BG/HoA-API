d61d96f6db7c3ea2d4e711fff9c2b8c23f5f435b
Rares Toader <89773299+RaresToader@users.noreply.github.com>
2022-12-19 13:25:41 +0100

    Add integration tests.

--- hoa-microservice/src/main/java/nl/tudelft/sem/template/hoa/controllers/HoaController.java
+++ hoa-microservice/src/main/java/nl/tudelft/sem/template/hoa/controllers/HoaController.java
@@ -6,14 +6,12 @@ import nl.tudelft.sem.template.hoa.db.RequirementService;
 import nl.tudelft.sem.template.hoa.domain.Hoa;
 import nl.tudelft.sem.template.hoa.models.HoaRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 /**
  * The controller for the association.
@@ -23,6 +21,7 @@ public class HoaController {
 
     private final transient HoaService hoaService;
     private final transient RequirementService requirementService;
+
 
     /**
      * Constructor for the HoaController.
@@ -50,7 +49,6 @@ public class HoaController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
-
     }
 
 
@@ -79,7 +77,7 @@ public class HoaController {
         try {
             return ResponseEntity.ok(hoaService.getHoaById(id));
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+            return ResponseEntity.badRequest().build();
         }
     }
 }
