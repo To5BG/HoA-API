2de8137c5c0cb7aff48ce524677d887b5ef65ab8
Alperen Guncan <A.Guncan@student.tudelft.nl>
2022-12-19 16:45:43 +0100

    Finalize converter test suites

---
+++ voting-microservice/src/test/java/voting/db/converter/LocalDateTimeConverterTest.java
@@ -0,0 +1,49 @@
+package voting.db.converter;
+
+import org.junit.jupiter.api.Test;
+import voting.annotations.TestSuite;
+import voting.db.converters.LocalDateTimeConverter;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static voting.annotations.TestSuite.TestType.UNIT;
+
+@TestSuite(testType = UNIT)
+class LocalDateTimeConverterTest {
+
+    LocalDateTimeConverter sut = new LocalDateTimeConverter();
+
+    @Test
+    void convertToDatabaseColumnTest() {
+        LocalDateTime ldt = null;
+        Timestamp res = sut.convertToDatabaseColumn(ldt);
+        assertNull(res);
+
+        ldt = LocalDateTime.of(LocalDate.of(1010, 10, 10),
+                LocalTime.of(10, 10, 10));
+        res = sut.convertToDatabaseColumn(ldt);
+        assertEquals("1010-10-10 10:10:10.0", res.toString());
+    }
+
+    @Test
+    void convertToEntityAttributeTest() {
+        Timestamp ts = null;
+        LocalDateTime ldt = sut.convertToEntityAttribute(ts);
+        assertNull(ldt);
+
+        //"2010-11-12 13:14:15.0 UTC +1";
+        ts = new Timestamp(1_289_567_655_000L);
+        ldt = sut.convertToEntityAttribute(ts);
+        assertEquals(15, ldt.getSecond());
+        assertEquals(14, ldt.getMinute());
+        assertEquals(14, ldt.getHour());
+        assertEquals(12, ldt.getDayOfMonth());
+        assertEquals(11, ldt.getMonthValue());
+        assertEquals(2010, ldt.getYear());
+    }
+}
---
+++ voting-microservice/src/test/java/voting/db/converter/VotesConverterTest.java
@@ -0,0 +1,52 @@
+package voting.db.converter;
+
+import org.junit.jupiter.api.Test;
+import voting.annotations.TestSuite;
+import voting.db.converters.VotesConverter;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static voting.annotations.TestSuite.TestType.UNIT;
+
+@TestSuite(testType = UNIT)
+class VotesConverterTest {
+    VotesConverter sut = new VotesConverter();
+
+    @Test
+    void convertToDatabaseColumnTest() {
+        Map<Integer, Integer> map = Map.of();
+        String res = sut.convertToDatabaseColumn(map);
+        assertEquals("", res);
+
+        map = Map.of(42, 69);
+        res = sut.convertToDatabaseColumn(map);
+        assertEquals("42=69", res);
+
+        map = Map.of(1, 1, 2, 0);
+        String finalRes = sut.convertToDatabaseColumn(map);
+        assertTrue(() -> finalRes.equals("1=1,2=0") || finalRes.equals("2=0,1=1"));
+    }
+
+    @Test
+    void convertToEntityAttributeTest() {
+        String testStr = "";
+        Map<Integer, Integer> map = sut.convertToEntityAttribute(testStr);
+        assertTrue(map.isEmpty());
+
+        testStr = "11=12";
+        map = sut.convertToEntityAttribute(testStr);
+        assertEquals(1, map.size());
+        assertTrue(map.containsKey(11));
+        assertEquals(12, map.get(11));
+
+        testStr = "1=2,3=4";
+        map = sut.convertToEntityAttribute(testStr);
+        assertEquals(2, map.size());
+        assertTrue(map.containsKey(1));
+        assertEquals(2, map.get(1));
+        assertTrue(map.containsKey(3));
+        assertEquals(4, map.get(3));
+    }
+}
