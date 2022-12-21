4fd4572d7b95681f0ac5b1ea43e23ff6d1944808
Michal Krkoska <m.krkoska@student.tudelft.nl>
2022-12-21 10:50:59 +0100

    Add utils test

---
+++ authentication-and-member-microservice/src/test/java/nl/tudelft/sem/template/authmember/utils/TimeUtilsTest.java
@@ -0,0 +1,20 @@
+package nl.tudelft.sem.template.authmember.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeUtilsTest {
+
+    @Test
+    void absoluteDifference() {
+        LocalDateTime start = LocalDateTime.now(ZoneOffset.UTC);
+        assertEquals(LocalDateTime.ofEpochSecond(2*3600,0, ZoneOffset.UTC),
+                TimeUtils.absoluteDifference(start, start.plusHours(1)));
+    }
+}
