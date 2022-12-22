1295cdd0c1ceb01c0b9d0024f0b394a3fbc3d23c
Michal Krkoska <m.krkoska@student.tudelft.nl>
2022-12-22 19:07:41 +0100

    Add generated annotation to config files

--- hoa-microservice/src/main/java/nl/tudelft/sem/template/hoa/config/RequestAuthenticationConfig.java
+++ hoa-microservice/src/main/java/nl/tudelft/sem/template/hoa/config/RequestAuthenticationConfig.java
@@ -1,5 +1,6 @@
 package nl.tudelft.sem.template.hoa.config;
 
+import nl.tudelft.sem.template.hoa.annotations.Generated;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 /**
  * The type Web security config.
  */
+@Generated
 @Configuration
 public class RequestAuthenticationConfig extends WebSecurityConfigurerAdapter {
 
